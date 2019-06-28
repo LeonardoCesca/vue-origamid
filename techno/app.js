3a8f224f088936a3029e7a1d89166dc46e8c30f2
LeonardoCesca <leonardocescaf@gmail.com>
2019-06-28 09:41:32 -0300

    ADD consuming api

--- techno/app.js
+++ techno/app.js
@@ -1,6 +1,18 @@
 const vm = new Vue({
     el: "#app",
     data: {
-        mensagem: "O Vue está funcionando"
+        produtos: []
+    },
+    methods: {
+        fetchProdutos() {
+            fetch("./api/produtos.json")
+                .then(response => response.json())
+                .then(response => {
+                    this.produtos = response;
+                })
+        }
+    },
+    created() {
+        this.fetchProdutos();
     }
 })
